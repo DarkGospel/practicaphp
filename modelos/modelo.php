40759ae8a645892a025ed0910e216562ed4099c7
juanma <gospel9616@gmail.com>
2019-01-29 15:47:57 +0100

    Arreglado error con los acentos

--- modelos/modelo.php
+++ modelos/modelo.php
@@ -31,6 +31,7 @@ class modelo {
     try {
       $this->conexion = new PDO("mysql:host=$this->host;dbname=$this->db", $this->user, $this->pass);
       $this->conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $this->conexion->query("SET NAMES 'utf8'");
       return TRUE;
     } catch (PDOException $ex) {
       return $ex->getMessage();
